1626c80ce6e408ed5603a87fdf6b69c2d14c8ace
abhishekshah3@gmail.com <abhishekshah3@gmail.com>
2019-04-01 10:00:57 +0530

    Initial Commit2

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import socket
-import _thread
+from _thread import *
 import sys
 
-server = ""
+server = "192.168.0.100"
 port = 10000 #randomly chosen
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #bind port and socket to server
@@ -11,12 +11,30 @@ try:
 except socket.error as e:
     str(e)
 s.listen(2) #for 2 people, optional,
+#if left blank, listens for unlimited connections
+
 print("Waiting for a connection, Server started. . . ")
 
 def threaded_client(conn):
-    pass
+    conn.send(str.encode("Connected"))
+    reply=""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+            if not data:
+                print("Disconnected . . .")
+                break
+            else:
+                print("Recieved : ", reply)
+                print("Sending: ", reply)
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
 
 
 while True:
     conn , addr = s.accept()
     print ("Connected to : ", addr)
+    start_new_thread(threaded_client, (conn,))
